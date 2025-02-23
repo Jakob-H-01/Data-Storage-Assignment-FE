50ed55b320c3748aa7845990b052953d05c62943
Jakob Hjalmarsson <jakob.hjalmarsson@utb.ecutbildning.se>
2025-02-23 14:14:10 +0100

    Added a vue project with complete routing and a header component

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <header>
+    <div class="wrapper">
+      <RouterLink class="logo" to="/">Datalagringsuppgift</RouterLink>
+      <nav class="nav">
+        <RouterLink to="/">Hem</RouterLink>
+        <RouterLink to="/projects">Alla Projekt</RouterLink>
+        <RouterLink to="/projects/new">Skapa Projekt</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-block: 2rem;
+}
+.nav {
+  display: flex;
+  gap: 3rem;
+}
+a {
+  font-size: 1.6rem;
+  color: var(--primary-text);
+
+  &:hover {
+    color: var(--primary);
+  }
+}
+.logo {
+  color: var(--primary);
+}
+.router-link-active {
+  color: var(--primary);
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import ProjectsView from '@/views/ProjectsView.vue'
+import ProjectView from '@/views/ProjectView.vue'
+import NewProjectView from '@/views/NewProjectView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: HomeView,
+    },
+    {
+      path: '/projects',
+      component: ProjectsView,
+    },
+    {
+      path: '/projects/:id',
+      component: ProjectView,
+    },
+    {
+      path: '/projects/new',
+      component: NewProjectView,
+    },
+  ],
+})
+
+export default router
